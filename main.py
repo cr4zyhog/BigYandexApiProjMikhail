e311e1d496b34d5ac01d5707324ad36dd2eb3a52
cr4zyhog <huiojiu@gmail.com>
2025-02-24 19:47:12 +0300

    upd 24.02.2025

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from PyQt6 import uic  # Импортируем uic
 from PyQt6.QtGui import QIntValidator, QPixmap
 from PyQt6.QtWidgets import QApplication, QMainWindow, QSlider
+from PyQt6.QtCore import Qt
 from io import BytesIO
 import requests
 from PIL import Image
@@ -14,6 +15,8 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('form.ui', self)  # Загружаем дизайн
         self.count_id = 0
+        self.delta = 1
+        self.delta_coord = 1
         self.initUI()
 
     def initUI(self):
@@ -24,26 +27,44 @@ class MyWidget(QMainWindow):
 
         self.dolgota_line.setValidator(only_int)
 
-        self.horizontalSlider.setTickInterval(300)
+        self.mashtab.setValidator(only_int)
 
         self.search_button.clicked.connect(self.show_im)
 
-        self.horizontalSlider.sliderReleased.connect(self.show_im)
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_PageUp:
+            self.delta -= 0.05
+            self.mashtab.setText(str(self.delta))
+            self.show_im()
+        if event.key() == Qt.Key.Key_PageDown:
+            self.delta += 0.05
+            self.mashtab.setText(str(self.delta))
+            self.show_im()
+        if event.key() == Qt.Key.Key_Right:
+            self.shirota_line.setText(str(float(self.shirota_line.text()) + (self.delta_coord * self.delta)))
+            self.show_im()
+        if event.key() == Qt.Key.Key_Left:
+            self.shirota_line.setText(str(float(self.shirota_line.text()) - (self.delta_coord * self.delta)))
+            self.show_im()
+        if event.key() == Qt.Key.Key_Up:
+            self.dolgota_line.setText(str(float(self.dolgota_line.text()) + (self.delta_coord * self.delta)))
+            self.show_im()
+        if event.key() == Qt.Key.Key_Down:
+            self.dolgota_line.setText(str(float(self.dolgota_line.text()) - (self.delta_coord * self.delta)))
+            self.show_im()
 
     def show_im(self):
         shirota = self.shirota_line.text()
         dolgota = self.dolgota_line.text()
 
-        delta = str(3 - self.horizontalSlider.value() / 100)
-        # apikey = "1254fb15-ad8f-4203-b8c5-131f37c0b498"
-        apikey = '5815d7d2-6bbe-424d-a32d-028b8c596fa2'
+        delta = str(self.delta)
+        apikey = "1254fb15-ad8f-4203-b8c5-131f37c0b498"
 
         # Собираем параметры для запроса к StaticMapsAPI:
         map_params = {
             "ll": f'{shirota},{dolgota}',
-            "spn": ",".join([delta, delta]),
+            "spn": ','.join([self.mashtab.text(), self.mashtab.text()]),
             "apikey": apikey,
-
         }
 
         map_api_server = "https://static-maps.yandex.ru/v1"
@@ -55,6 +76,7 @@ class MyWidget(QMainWindow):
         opened_image = QPixmap(fileie)
         self.karta.setPixmap(opened_image)
         os.remove(fileie)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
